23400259840c2fd749cf16a4a5998f0a9d60376e
Ken Fischer <kjfischer5@gmail.com>
2022-03-14 18:21:24 -0400

    Initial Commit

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace howto_weather_forecast2
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.btnForecast = new System.Windows.Forms.Button();
+            this.lvwForecast = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblAlarm = new System.Windows.Forms.Label();
+            this.btnAlarm = new System.Windows.Forms.Button();
+            this.imgWeather = new System.Windows.Forms.PictureBox();
+            this.dtpAlarm = new System.Windows.Forms.DateTimePicker();
+            this.chkSound = new System.Windows.Forms.CheckBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblTempData = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblZip = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imgWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Forecast:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(52, 170);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(138, 20);
+            this.txtLocation.TabIndex = 15;
+            this.txtLocation.Text = "20500";
+            this.txtLocation.TextChanged += new System.EventHandler(this.txtLocation_TextChanged);
+            // 
+            // btnForecast
+            // 
+            this.btnForecast.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForecast.Location = new System.Drawing.Point(12, 196);
+            this.btnForecast.Name = "btnForecast";
+            this.btnForecast.Size = new System.Drawing.Size(327, 36);
+            this.btnForecast.TabIndex = 16;
+            this.btnForecast.Text = "Get Forecast";
+            this.btnForecast.UseVisualStyleBackColor = true;
+            this.btnForecast.Click += new System.EventHandler(this.btnForecast_Click);
+            // 
+            // lvwForecast
+            // 
+            this.lvwForecast.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvwForecast.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvwForecast.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvwForecast.HideSelection = false;
+            this.lvwForecast.Location = new System.Drawing.Point(12, 261);
+            this.lvwForecast.Name = "lvwForecast";
+            this.lvwForecast.Size = new System.Drawing.Size(684, 337);
+            this.lvwForecast.TabIndex = 19;
+            this.lvwForecast.UseCompatibleStateImageBehavior = false;
+            this.lvwForecast.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Day";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Time";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Temp.";
+            this.columnHeader3.Width = 70;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Chance";
+            this.columnHeader4.Width = 70;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Type";
+            this.columnHeader5.Width = 70;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Cloud Cover";
+            this.columnHeader6.Width = 100;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Condition";
+            this.columnHeader7.Width = 150;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Alarm:";
+            // 
+            // lblAlarm
+            // 
+            this.lblAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAlarm.Location = new System.Drawing.Point(508, 86);
+            this.lblAlarm.Name = "lblAlarm";
+            this.lblAlarm.Size = new System.Drawing.Size(189, 87);
+            this.lblAlarm.TabIndex = 36;
+            // 
+            // btnAlarm
+            // 
+            this.btnAlarm.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAlarm.Location = new System.Drawing.Point(367, 86);
+            this.btnAlarm.Name = "btnAlarm";
+            this.btnAlarm.Size = new System.Drawing.Size(123, 87);
+            this.btnAlarm.TabIndex = 37;
+            this.btnAlarm.Text = "Set Alarm";
+            this.btnAlarm.UseVisualStyleBackColor = true;
+            this.btnAlarm.Click += new System.EventHandler(this.btnAlarm_Click);
+            // 
+            // imgWeather
+            // 
+            this.imgWeather.BackgroundImage = global::howto_weather_forecast2.Properties.Resources.blue_square;
+            this.imgWeather.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgWeather.Location = new System.Drawing.Point(15, 26);
+            this.imgWeather.Name = "imgWeather";
+            this.imgWeather.Size = new System.Drawing.Size(100, 100);
+            this.imgWeather.TabIndex = 38;
+            this.imgWeather.TabStop = false;
+            // 
+            // dtpAlarm
+            // 
+            this.dtpAlarm.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpAlarm.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtpAlarm.Location = new System.Drawing.Point(367, 42);
+            this.dtpAlarm.Name = "dtpAlarm";
+            this.dtpAlarm.ShowUpDown = true;
+            this.dtpAlarm.Size = new System.Drawing.Size(191, 32);
+            this.dtpAlarm.TabIndex = 39;
+            // 
+            // chkSound
+            // 
+            this.chkSound.AutoSize = true;
+            this.chkSound.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkSound.Location = new System.Drawing.Point(221, 168);
+            this.chkSound.Name = "chkSound";
+            this.chkSound.Size = new System.Drawing.Size(118, 20);
+            this.chkSound.TabIndex = 41;
+            this.chkSound.Text = "Audio Forecast";
+            this.chkSound.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(130, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(163, 38);
+            this.label8.TabIndex = 42;
+            this.label8.Text = "Weather Data Over Next 12 Hours:";
+            // 
+            // lblTempData
+            // 
+            this.lblTempData.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTempData.Location = new System.Drawing.Point(133, 64);
+            this.lblTempData.Name = "lblTempData";
+            this.lblTempData.Size = new System.Drawing.Size(160, 90);
+            this.lblTempData.TabIndex = 43;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(573, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 32);
+            this.button2.TabIndex = 44;
+            this.button2.Text = "Monitor Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblZip
+            // 
+            this.lblZip.AutoSize = true;
+            this.lblZip.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZip.Location = new System.Drawing.Point(12, 170);
+            this.lblZip.Name = "lblZip";
+            this.lblZip.Size = new System.Drawing.Size(34, 18);
+            this.lblZip.TabIndex = 45;
+            this.lblZip.Text = "ZIP:";
+            // 
+            // FrmMain
+            // 
+            this.AcceptButton = this.btnForecast;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 610);
+            this.Controls.Add(this.lblZip);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblTempData);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.chkSound);
+            this.Controls.Add(this.dtpAlarm);
+            this.Controls.Add(this.imgWeather);
+            this.Controls.Add(this.btnAlarm);
+            this.Controls.Add(this.lblAlarm);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lvwForecast);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.btnForecast);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Forecast Alarm Clock";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgWeather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.Button btnForecast;
+        private System.Windows.Forms.ListView lvwForecast;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblAlarm;
+        private System.Windows.Forms.Button btnAlarm;
+        private System.Windows.Forms.PictureBox imgWeather;
+        private System.Windows.Forms.DateTimePicker dtpAlarm;
+        private System.Windows.Forms.CheckBox chkSound;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTempData;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblZip;
+    }
+}
+
